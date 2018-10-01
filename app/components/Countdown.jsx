7cb8b3be99809c56ac2da8356951b098d603d9d1
NBowes <nathen.bowes@shopify.com>
2018-10-01 12:50:34 -0700

    add countdown form

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -1,13 +1,25 @@
 var React = require('react');
 var Clock = require('Clock');
+var CountdownForm = require('CountdownForm');
 
 var Countdown = React.createClass({
+    getInitialState: function(){
+        return { 
+            count: 0
+        };
+    },
+    handleCountdown: function(time){
+        this.setState({
+            count: time
+        });
+    },
     render: function(){
+        var {count} = this.state;
         return(
             <div>
-                <Clock totalSeconds={0}/>
-                Counter.jsx
-            </div>
+                <Clock totalSeconds={count}/>
+                <CountdownForm onSetCountdown={this.handleCountdown}/>
+            </div> 
         )
     }
 });
---
+++ app/components/CountdownForm.jsx
@@ -0,0 +1,25 @@
+var React = require('react');
+
+var CountdownForm = React.createClass({
+    onSubmit: function(e){
+        e.preventDefault();
+        var time = this.refs.seconds.value;
+        if(time.match(/^[0-9]*$/)){
+            this.refs.seconds.value = "";
+            this.props.onSetCountdown(parseInt(time,));
+        }
+    },
+    render: function(){
+        return(
+            <div>
+                    <form onSubmit={this.onSubmit}>
+                            <input type="text" ref="seconds" placeholder="Enter time in seconds"/>
+                            <button className="button expanded">Submit</button>
+                    </form> 
+            </div>
+        )
+
+    }
+})
+
+module.exports = CountdownForm;
